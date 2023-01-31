e38e6b09eb95c8c6938c1a507998d35959218fc0
Kves <vnlkbn@gmail.com>
2023-01-31 23:38:03 +0100

    home page draft

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +10,74 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Image.asset('lib/assets/images/reading_man.png'),
+            SingleChildScrollView(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Column(
+                  children: [
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                    BookTileWidget(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BookTileWidget extends StatelessWidget {
+  const BookTileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.green.withOpacity(0.5),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Great Book'),
+            Text('John Doe'),
+            Text('Pages: 245'),
+          ],
+        ),
+      ),
+    );
   }
 }
