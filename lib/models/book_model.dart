6abc2eee96981b714e3cb90ff8d7d3c217b95b68
Kves <vnlkbn@gmail.com>
2023-02-01 17:35:11 +0100

    added book model class

---
+++ lib/models/book_model.dart
@@ -0,0 +1,17 @@
+class BookModel {
+  final String bookTitle;
+  final String? bookAuthor;
+  final String? bookPages;
+  final String? bookRating;
+  final String? bookAddedDate;
+  final String? bookCoverFromApi;
+
+  BookModel({
+    required this.bookTitle,
+    this.bookAuthor,
+    this.bookPages,
+    this.bookRating,
+    this.bookAddedDate,
+    this.bookCoverFromApi,
+  });
+}
