eccd104e5641ac6203237606a2df1dab240f72cd
Kves <vnlkbn@gmail.com>
2023-02-08 15:21:11 +0100

    l10n translations in Polish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:read_app/data/book_data.dart';
 import 'package:read_app/pages/main_page.dart';
