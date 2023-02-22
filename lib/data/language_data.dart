e10aa40d36ebe8b1263d9dc022abd0cc06af821e
Kves <vnlkbn@gmail.com>
2023-02-23 00:10:56 +0100

    deafult language set to en

--- lib/data/language_data.dart
+++ lib/data/language_data.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LanguageData extends ChangeNotifier {
-  Locale currentLocale = const Locale('pl');
+  Locale currentLocale = const Locale('en');
 
   void changeLocale(String locale) {
     currentLocale = Locale(locale);
